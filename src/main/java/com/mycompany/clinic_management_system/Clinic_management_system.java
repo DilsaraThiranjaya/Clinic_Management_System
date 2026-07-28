3304c65e8ecfe1f7848ad37a4a2414321cc2ee3e
Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2026-07-28 15:51:49 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/clinic_management_system/Clinic_management_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinic_management_system;
+
+/**
+ *
+ * @author MSI
+ */
+public class Clinic_management_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
